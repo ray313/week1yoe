1879d0e0c0eb893314179c6ecd2b68433008f453
ray313 <52668268+ray313@users.noreply.github.com>
2019-07-09 13:47:22 -0400

    Add files via upload

--- warmUp.java
+++ warmUp.java
@@ -1,9 +1,26 @@
 package day2;
 
+import java.util.Scanner;
+
 public class warmUp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		Scanner Ray = new Scanner(System.in);
+		System.out.println("whats your name");
+		Ray.next();
+		System.out.println("age");
+		Ray.nextInt();
+		System.out.println("your favorite color");
+		Ray.next();
+		System.out.println("best number");
+		Ray.nextInt();
+		System.out.println("Thank you for your particaption");
+		
+		Ray.close();
+		
+				
 
 	}
 
